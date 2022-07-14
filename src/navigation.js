7234052b3eadc06da9c0d3c07c5d7a9d0b64b41b
Jesus Vargas <jesusvarguer18@gmail.com>
2022-07-13 19:41:19 -0500

    buscador

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,5 @@
 searchFormBtn.addEventListener('click', () => {
-  location.hash = '#search=';
+  location.hash = '#search=' + searchFormInput.value;
 });
 
 trendingBtn.addEventListener('click', () => {
@@ -10,12 +10,13 @@ arrowBtn.addEventListener('click', () => {
   location.hash = '#home';
 });
 
+
 window.addEventListener('DOMContentLoaded', navigator, false);
 window.addEventListener('hashchange', navigator, false);
 
 function navigator() {
   console.log({ location });
-  
+
   if (location.hash.startsWith('#trends')) {
     trendsPage();
   } else if (location.hash.startsWith('#search=')) {
@@ -27,6 +28,9 @@ function navigator() {
   } else {
     homePage();
   }
+
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 function homePage() {
@@ -99,13 +103,17 @@ function searchPage() {
   arrowBtn.classList.remove('inactive');
   arrowBtn.classList.remove('header-arrow--white');
   headerTitle.classList.add('inactive');
-  headerCategoryTitle.classList.remove('inactive');
+  headerCategoryTitle.classList.add('inactive');
   searchForm.classList.remove('inactive');
 
   trendingPreviewSection.classList.add('inactive');
   categoriesPreviewSection.classList.add('inactive');
   genericSection.classList.remove('inactive');
   movieDetailSection.classList.add('inactive');
+
+  // ['#search', 'buscado']
+  const [_, query] = location.hash.split('=');
+  getMoviesBySearch(query);
 }
 
 function trendsPage() {

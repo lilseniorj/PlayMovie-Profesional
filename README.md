7d8d8411cc93313ba4cbec4a189e688113f55c0e
Jesus Vargas <jesusvarguer18@gmail.com>
2022-07-18 21:12:40 -0500

    ultima actualizacion

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # curso-api-rest-javascript-practico
+
+## Que se implemento en el repositorio
+- se aplico conocimiento del manejo de las API con JavaScript y conocimientos basicos de HTML y CSS
--- src/nodes.js
+++ src/nodes.js
@@ -25,3 +25,5 @@ const trendingBtn = document.querySelector('.trendingPreview-btn');
 const movieDetailTitle = document.querySelector('.movieDetail-title');
 const movieDetailDescription = document.querySelector('.movieDetail-description');
 const movieDetailScore = document.querySelector('.movieDetail-score');
+
+// LilSeniorj
